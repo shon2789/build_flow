cf02875cad594c036cfa69faed4d51a310368291
Shon Shinkarenko <shon2789@gmail.com>
2021-09-23 18:30:50 +0300

    Added editor section

--- frontend/src/editable-cmps/WebAppHeaderCmp.jsx
+++ frontend/src/editable-cmps/WebAppHeaderCmp.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 import { DynamicCmp } from '../components/DynamicCmp'
 
 export const WebAppHeaderCmp = ({ cmp }) => {
-
-
     return (
         <header style={cmp.attributes.style} className={cmp.attributes.className}>
             <h1>{cmp.info.title}</h1>
+            <h3>{cmp.info.subtitle}</h3>
             {cmp.children && cmp.children.map((child, idx) => <DynamicCmp key={idx} cmp={child} />)}
-            <h3>{cmp.info.shatz}</h3>
         </header>
     )
 }
