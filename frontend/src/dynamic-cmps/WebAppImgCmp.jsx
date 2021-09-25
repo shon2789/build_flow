ad969281c0d2d2fcc410d3bbd1621f8fab39cbe7
RazAbk <RazAb98754@Gmail.com>
2021-09-25 14:30:09 +0300

    working on imgCmp

--- frontend/src/dynamic-cmps/WebAppImgCmp.jsx
+++ frontend/src/dynamic-cmps/WebAppImgCmp.jsx
@@ -5,9 +5,9 @@ export const WebAppImgCmp = ({ cmp, currCmp, onSetCurrCmp }) => {
 
     if (currCmp && currCmp.id === cmp.id) {
         return (
-            <div className="nuetral-div">
+            <div className="nuetral-div" style={{ ...cmp.attributes.style, position: 'relative', outline: '2px dashed #c6c6c6' }}>
                 <ElementToolBar />
-                <img id={cmp.id} style={{ ...cmp.attributes.style, position: 'relative', outline: '2px dashed #c6c6c6' }} className={cmp.attributes.className} src={cmp.attributes.src} alt="sorry no img" onClick={(ev) => onSetCurrCmp(ev, cmp)} />
+                <img id={cmp.id} style={{ ...cmp.attributes.style, width: '100%', height: '100%', margin: '0', padding: '0' }} className={cmp.attributes.className} src={cmp.attributes.src} alt="sorry no img" onClick={(ev) => onSetCurrCmp(ev, cmp)} />
             </div>
         )
     }
