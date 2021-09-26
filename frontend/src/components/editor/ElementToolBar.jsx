539d0ad7c36c2870ccbc74162b41cb45ae272799
Shon Shinkarenko <shon2789@gmail.com>
2021-09-26 12:14:58 +0300

    Added tooltips to element tool bar

--- frontend/src/components/editor/ElementToolBar.jsx
+++ frontend/src/components/editor/ElementToolBar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { FaTrashAlt } from "react-icons/fa";
 import { FaCopy } from "react-icons/fa";
+import Tooltip from '@mui/material/Tooltip';
 
 
 
 export const ElementToolBar = ({ cmp, onDeleteCmp, onDuplicateCmp }) => {
     return (
         <div className="element-tool-bar">
-            <div className="element-tool" onClick={() => { onDuplicateCmp(cmp) }}><FaCopy className="copy-tool" /></div>
-            <div className="element-tool" onClick={() => { onDeleteCmp(cmp.id) }}><FaTrashAlt className="delete-tool" /></div>
+            <Tooltip title="Copy" arrow placement="top"><div className="element-tool" onClick={() => { onDuplicateCmp(cmp) }}><FaCopy className="copy-tool" /></div></Tooltip>
+            <Tooltip title="Delete" arrow placement="top"><div className="element-tool" onClick={() => { onDeleteCmp(cmp.id) }}><FaTrashAlt className="delete-tool" /></div></Tooltip>
         </div>
     )
 }
