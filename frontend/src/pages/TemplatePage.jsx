41a78f4a21851c55b450fadf6a21fe16cf0cf6d8
Shon Shinkarenko <shon2789@gmail.com>
2021-09-27 23:14:58 +0300

    Fixed template page scroll issue

--- frontend/src/pages/TemplatePage.jsx
+++ frontend/src/pages/TemplatePage.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { TemplateList } from '../components/templates/TemplateList'
 
 export const TemplatePage = () => {
+
+    if (window.innerWidth < 850) {
+        window.scrollTo({ top: 0 })
+    }
+
     return (
         <TemplateList />
     )
