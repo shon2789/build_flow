b1ea7a53b99c90c330fef54c83ed441fd64941ed
Jonathan Geiger <tantangeiger@gmail.com>
2021-09-30 22:09:42 +0300

    user page now fully responsive

--- frontend/src/components/user/UserSites.jsx
+++ frontend/src/components/user/UserSites.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { UserDetails } from './UserDetails'
 import { UserSiteList } from './UserSiteList'
 import { UserTopNav } from './UserTopNav'
-
-export const UserSites = ({windowWidth}) => {
+import Divider from "@material-ui/core/Divider";
+export const UserSites = ({ windowWidth }) => {
     return (
         <section className="user-sites">
-            {(windowWidth < 1130 ) && <UserDetails /> }
+            {(windowWidth < 1130) && <UserDetails />}
+            {(windowWidth < 1130) && <Divider style={{ width: '100%' }} variant="fullWidth" />}
+
             <UserTopNav />
             <UserSiteList />
         </section>
