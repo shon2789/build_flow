c7d5acb00aa4812a84849ad05c75523c95e12353
Jonathan Geiger <tantangeiger@gmail.com>
2021-10-03 00:29:23 +0300

    added user sites filter

--- frontend/src/components/user/UserSites.jsx
+++ frontend/src/components/user/UserSites.jsx
@@ -3,15 +3,15 @@ import { UserDetails } from './UserDetails'
 import { UserSiteList } from './UserSiteList'
 import { UserTopNav } from './UserTopNav'
 import Divider from "@material-ui/core/Divider";
-export const UserSites = ({ windowWidth, user }) => {
+export const UserSites = ({ windowWidth, user, filterUserSites, filterBy }) => {
 
     return (
         <section className="user-sites">
             {(windowWidth < 1130) && <UserDetails windowWidth={windowWidth} />}
             {(windowWidth < 1130) && <Divider style={{ width: '100%' }} variant="fullWidth" />}
 
-            <UserTopNav />
-            <UserSiteList user={user} />
+            <UserTopNav filterBy={filterBy} filterUserSites={filterUserSites} />
+            <UserSiteList filterBy={filterBy} user={user} />
         </section>
     )
 }
--- frontend/src/components/user/UserTopNav.jsx
+++ frontend/src/components/user/UserTopNav.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export const UserTopNav = () => {
+export const UserTopNav = ({ filterUserSites, filterBy }) => {
     return (
         <div className="user-top-nav">
             <div className="user-top-links">
 
-                <button>All Sites</button>
-                <button>Published</button>
-                <button>Saved</button>
+                <button className={filterBy === 'all' ? 'active' : ''} onClick={() => { filterUserSites('all') }}>All Sites</button>
+                <button className={filterBy === 'published' ? 'active' : ''} onClick={() => { filterUserSites('published') }}>Published</button>
+                <button className={filterBy === 'saved' ? 'active' : ''} onClick={() => { filterUserSites('saved') }}>Saved</button>
             </div>
         </div>
     )
