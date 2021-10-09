bba2952c6056cf17e8339a2ab263d7f3e4b5d44d
Shon Shinkarenko <shon2789@gmail.com>
2021-10-09 11:21:06 +0300

    Cleaned cursor component code

--- frontend/src/components/Cursor.jsx
+++ frontend/src/components/Cursor.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 import { FiMousePointer } from "react-icons/fi";
 
 export const Cursor = ({ pos }) => {
-
-    console.log(pos, "anotehr mennnn siopdermannnn");
-
     return (
-
         <FiMousePointer style={{ position: "fixed", top: `${pos.y}`, left: `${pos.x}` }} />
     )
 }
