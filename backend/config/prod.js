f00944085472426ad17520824d3ba39a1309c20d
Shon Shinkarenko <shon2789@gmail.com>
2021-10-09 19:07:13 +0300

    Connected atals DB

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  'dbURL': `mongodb+srv://Shon2789:${process.env.MONGO_PASSWORD}@buildflow.thcev.mongodb.net/test`,
 }
