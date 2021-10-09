83e0d5ccf0e4f6da4a75e5c69dddf73b78ad95f4
Jonathan Geiger <tantangeiger@gmail.com>
2021-10-09 10:25:57 +0300

    footer changed

--- frontend/src/components/home-page/Footer.jsx
+++ frontend/src/components/home-page/Footer.jsx
@@ -1,36 +1,17 @@
 import React from 'react'
-import { TiSocialLinkedin } from "react-icons/ti";
-import { FiGithub } from "react-icons/fi";
+
+
+import { AiFillCopyrightCircle } from "react-icons/ai";
 
 
 
 export const Footer = () => {
     return (
         <footer className="footer-container">
-            <div className="us-container">
-                <div className="master-container">
-                    <h3>Shon Shinkarenko</h3>
-                    <div className="footer-icons-container">
-                        <a href="https://github.com/shon2789" rel="noopener noreferrer" target="_blank" ><FiGithub className="icon-btn" /></a>
-                        <a href="https://www.linkedin.com/in/shon-shinkarenko/" rel="noopener noreferrer" target="_blank" ><TiSocialLinkedin className="icon-btn" /></a>
-                    </div>
-
-                </div>
-                <div className="master-container">
-                    <h3>Jonathan Geiger</h3>
-                    <div className="footer-icons-container">
-                        <a href="https://github.com/geiger01" rel="noopener noreferrer" target="_blank" ><FiGithub className="icon-btn" /></a>
-                        <a href="https://www.linkedin.com/in/jonathan-geiger/" rel="noopener noreferrer" target="_blank" ><TiSocialLinkedin className="icon-btn" /></a>
-                    </div>
-                </div>
-                <div className="master-container">
-                    <h3>Raz Abekasis</h3>
-                    <div className="footer-icons-container">
-                        <a href="https://github.com/RazAbk" rel="noopener noreferrer" target="_blank" ><FiGithub className="icon-btn" /></a>
-                        <a href="https://www.linkedin.com/in/raz-abekasis/" rel="noopener noreferrer" target="_blank" ><TiSocialLinkedin className="icon-btn" /></a>
-                    </div>
-                </div>
+            <div className="copyright-container">
+                <AiFillCopyrightCircle />
             </div>
+            <h3>All rights reserved to BuildFlow</h3>
         </footer>
     )
 }
